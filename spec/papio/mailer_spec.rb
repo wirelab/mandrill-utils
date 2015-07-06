0c3e7ae354581a826899c66e292ebda9568b1dab
Tim Peters <tim@wirelab.nl>
2015-07-06 13:08:56 +0200

    Add default to spec

--- spec/papio/mailer_spec.rb
+++ spec/papio/mailer_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 module Papio
   describe Mailer do
     class ExampleMailer < Mailer
+      default subject: 'The Subject'
       def an_email
         @title = 'Hello, World!'
         mail to: 'test@example.com', template: 'test-template'
@@ -13,6 +14,7 @@ module Papio
       expect(mail.to[:email]).to eq 'test@example.com'
       expect(mail.template).to eq 'test-template'
       expect(mail.vars).to eq({title: 'Hello, World!'})
+      expect(mail.subject).to eq "The Subject"
     end
   end
 end
